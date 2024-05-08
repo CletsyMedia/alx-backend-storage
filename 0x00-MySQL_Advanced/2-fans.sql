9ddd11190e903398278a73fad54593d52bf9b597
CletsyMedia <Cletussam12@yahoo.com>
2024-05-08 23:06:39 +0100

    Best band ever

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Rank country origins of bands by the number of (non-unique) fans
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
