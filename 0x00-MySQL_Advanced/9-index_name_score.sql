2878d18abe5e23cf66bdf5399b8bd883931320cb
CletsyMedia <Cletussam12@yahoo.com>
2024-05-10 01:23:15 +0100

    Indexing

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,5 +1,4 @@
---Creates an idx name first score on a table
+--Creates an idx_name_first_score on a table
 CREATE INDEX idx_name_first_score
---Target the first letter of name and the score
 ON names(name(1), score);
 
