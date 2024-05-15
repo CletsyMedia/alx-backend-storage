46471d69d95ef4c34629e83df185fadecdec691a
CletsyMedia <Cletussam12@yahoo.com>
2024-05-16 00:42:31 +0100

    Expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,47 +1,39 @@
 #!/usr/bin/env python3
-"""
-A module with tools for request caching and tracking.
-"""
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
-# Initialize Redis client
+
 redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
 def data_cacher(method: Callable) -> Callable:
-    """
-    Decorator to cache the output of fetched data and track the
-    number of times a URL was accessed.
-    """
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
-    def wrapper(url: str) -> str:
-        """
-        Wrapper function for caching the output and tracking the request.
-        """
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
+        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
-    return wrapper
+    return invoker
 
 
 @data_cacher
 def get_page(url: str) -> str:
-    """
-    Fetches the content of a URL using requests module and
-    caches the response.
-    """
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
     return requests.get(url).text
 
-
-if __name__ == "__main__":
-    # Example usage
-    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/https://www.example.com"
-    html_content = get_page(url)
-    print(html_content)
