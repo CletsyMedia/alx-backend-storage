29630d9b05c9c6c7423e1d72eba101c4cf1d61a3
CletsyMedia <Cletussam12@yahoo.com>
2024-05-16 01:04:49 +0100

    expiring cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,52 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
-import redis
+"""
+A module for implementing an expiring web cache and tracker.
+"""
+
 import requests
-from functools import wraps
+import redis
 from typing import Callable
 
-
+# Initialize Redis client
 redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
+def cache_and_track(method: Callable) -> Callable:
+    """
+    Decorator to cache the output of fetched data and track URL accesses.
+    """
+    def wrapper(url: str) -> str:
+        """
+        Wrapper function for caching and tracking URL accesses.
+        """
+        # Track URL access count
         redis_store.incr(f'count:{url}')
+
+        # Check if URL content is cached
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+
+        # Fetch URL content
+        response = method(url)
+        content = response.text
+
+        # Cache URL content with expiration time of 10 seconds
+        redis_store.setex(f'result:{url}', 10, content)
+
+        return content
+
+    return wrapper
 
 
-@data_cacher
+@cache_and_track
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """
+    Retrieves the HTML content of a URL and returns it.
+    """
+    return requests.get(url)
+
+
+if __name__ == "__main__":
+    # Example usage
+    print(get_page("http://slowwly.robertomurray.co.uk/delay/1000/url/https://example.com"))
